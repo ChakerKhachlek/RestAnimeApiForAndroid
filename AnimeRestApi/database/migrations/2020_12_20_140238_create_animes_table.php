0ecc9073d33d76cc6f9e2f60c07199d836ff477b
ChakerKhachlek <chaker-@hotmail.com>
2020-12-28 22:29:18 +0100

    database change to fit azure

--- AnimeRestApi/database/migrations/2020_12_20_140238_create_animes_table.php
+++ AnimeRestApi/database/migrations/2020_12_20_140238_create_animes_table.php
@@ -14,7 +14,7 @@ class CreateAnimesTable extends Migration
     public function up()
     {
         Schema::create('animes', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string("name");
             $table->TEXT("description", 255);
             $table->float("rating");
--- AnimeRestApi/database/migrations/2020_12_23_192657_create_user_anime_table.php
+++ AnimeRestApi/database/migrations/2020_12_23_192657_create_user_anime_table.php
@@ -14,7 +14,7 @@ class CreateUserAnimeTable extends Migration
     public function up()
     {
         Schema::create('user_anime', function (Blueprint $table) {
-            $table->id();
+           $table->increments('id');
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')
               ->references('id')
